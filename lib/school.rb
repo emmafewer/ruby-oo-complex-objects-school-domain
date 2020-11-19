958da8f7458b9a009be203ae491a3f72b21565f0
emmafewer <emmafewer20@gmail.com>
2020-11-18 20:35:04 -0800

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+class School
+
+    def initialize(school)
+       @roster = {}  
+    end 
+    
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade) 
+        if @roster[grade]
+            @roster[grade] << student
+        else 
+            @roster[grade]=[] << student
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.sort_by{|k,v|v.sort!}.to_h
+    end
+end
